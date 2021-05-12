a7859229459928c8e7dc66748088d475c7af2661
Avibah <58798677+Avibah@users.noreply.github.com>
2021-05-12 13:09:56 -0500

    Add Map Diagnoser

---
+++ SS Multi Tool/Map Diagnoser.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Map_Diagnoser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Map_Diagnoser));
+            this.Paste = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Diagnose = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(80, 3);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(70, 38);
+            this.Paste.TabIndex = 33;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(3, 47);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(147, 78);
+            this.Input.TabIndex = 32;
+            this.Input.Text = "";
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 3);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 38);
+            this.Open.TabIndex = 31;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Diagnose
+            // 
+            this.Diagnose.Location = new System.Drawing.Point(3, 132);
+            this.Diagnose.Name = "Diagnose";
+            this.Diagnose.Size = new System.Drawing.Size(147, 23);
+            this.Diagnose.TabIndex = 34;
+            this.Diagnose.Text = "Diagnose";
+            this.Diagnose.UseVisualStyleBackColor = true;
+            this.Diagnose.Click += new System.EventHandler(this.Diagnose_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(156, 3);
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.Size = new System.Drawing.Size(204, 152);
+            this.Output.TabIndex = 35;
+            this.Output.Text = "";
+            // 
+            // Map_Diagnoser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 159);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Diagnose);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Map_Diagnoser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Diagnose;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox Output;
+    }
+}
---
+++ SS Multi Tool/Map Diagnoser.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+
+
+namespace SS_Multi_Tool
+{
+    public partial class Map_Diagnoser : Form
+    {
+        public Map_Diagnoser()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Diagnose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool charlim = false;
+                bool fiveklim = false;
+                bool tenklim = false;
+                bool faroffgrid = false;
+                List<decimal> offgridtimes = new List<decimal>();
+                bool earlynote = false;
+                bool latenote = false;
+                bool audioerror = false;
+
+                string data = Input.Text;
+
+                if (data.Length > 16384)
+                {
+                    charlim = true;
+                }
+
+                string audioID = data.Substring(0, data.IndexOf(","));
+                data = data.Replace(audioID + ",", "");
+                string[] newdata = data.Split(',');
+
+                if (newdata.Length > 5000)
+                {
+                    fiveklim = true;
+                }
+                if (newdata.Length > 10000)
+                {
+                    tenklim = true;
+                }
+
+                decimal x;
+                decimal y;
+                decimal time;
+                int i = 0;
+                string output = "";
+
+                string directory = Directory.GetCurrentDirectory();
+                try
+                {
+                    string dir = directory + "\\" + audioID + ".mp3";
+                    if (!File.Exists(dir))
+                    {
+                        using (var wc = new SecureWebClient())
+                        {
+                            wc.DownloadFile("https://assetdelivery.roblox.com/v1/asset/?id=" + audioID, dir);
+                            File.Delete(dir);
+                        }
+                    }
+                }
+                catch
+                {
+                    audioerror = true;
+                }
+
+                foreach (var line in newdata)
+                {
+                    var lineSplit = Regex.Matches(line, "([^|]+)");
+                    x = decimal.Parse(lineSplit[0].Value);
+                    y = decimal.Parse(lineSplit[1].Value);
+                    time = decimal.Parse(lineSplit[2].Value);
+
+                    if (x < decimal.Parse("-0.875") || x > decimal.Parse("2.875") || y < decimal.Parse("-0.875") || y > decimal.Parse("2.875"))
+                    {
+                        faroffgrid = true;
+                        offgridtimes.Add(time);
+                    }
+                    if (time < 0)
+                    {
+                        earlynote = true;
+                    }
+                    if (i + 1 == newdata.Length)
+                    {
+                        var lineSplit2 = Regex.Matches(newdata[i - 1], "([^|]+)");
+                        decimal time2 = decimal.Parse(lineSplit2[2].Value);
+                        if (time > decimal.Parse("1.5") * time2)
+                        {
+                            latenote = true;
+                        }
+                    }
+
+                    i += 1;
+                }
+
+                if (audioerror == true)
+                {
+                    output += "\n\nUnable to download sound data, check if the audio was taken down";
+                }
+                if (charlim == true)
+                {
+                    output += "\n\nFile is over text box character limit, use gist.github.com to create a gist to use";
+                }
+                if (fiveklim == true)
+                {
+                    output += "\n\nFile is over 5 thousand notes, trim it down to 5 thousand if you don't own the 'Not Enough Notes' gamepass";
+                }
+                if (tenklim == true)
+                {
+                    output += "\n\nFile is over 10 thousand notes, trim it down to 10 thousand";
+                }
+                if (earlynote == true)
+                {
+                    output += "\n\nA note is placed before the song begins";
+                }
+                if (latenote == true)
+                {
+                    output += "\n\nA note may be after the song ends, check this in the editor before continuing";
+                }
+                if (faroffgrid == true)
+                {
+                    output += "\n\nOne or multiple notes are outside the offgrid borders, fix the offgrid distance of the notes at the timestamps listed below\n";
+                    foreach (var line in offgridtimes)
+                    {
+                        output += "\n" + line;
+                    }
+                }
+
+                Output.Text = output.Substring(2, output.Length - 2);
+                MessageBox.Show("Check the output box for steps in fixing map data. Some may not always apply to the map.\n\nCheck whether there are notes after the end of the song.\n\nIf none of the following steps solve the problem, check whether the audio was bypassed, as ROBLOX may stop audios from playing ingame but not take them off the website.");
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
