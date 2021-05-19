7a07b4ece04efe0b200072662af3d7817817280b
Avibah <58798677+Avibah@users.noreply.github.com>
2021-05-19 15:38:49 -0500

    Add new update message

---
+++ SS Multi Tool/Update.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace SS_Multi_Tool
+{
+    public partial class Update : Form
+    {
+        public string version;
+        public Update(string changelog, string vers)
+        {
+            InitializeComponent();
+            richTextBox1.Text = changelog;
+            version = vers;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Avibah/Sound-Space-Multi-Tool/releases/v" + version);
+        }
+    }
+}
