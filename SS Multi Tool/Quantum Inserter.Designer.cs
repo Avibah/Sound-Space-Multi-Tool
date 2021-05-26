4010350b66763c436b6d28badcf1d9352cb57565
Avibah <58798677+Avibah@users.noreply.github.com>
2021-05-26 01:07:55 -0500

    Add Quantum Inserter

---
+++ SS Multi Tool/Quantum Inserter.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Quantum_Inserter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Quantum_Inserter));
+            this.Paste = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SetLimits = new System.Windows.Forms.CheckBox();
+            this.SectionEnd = new System.Windows.Forms.TextBox();
+            this.SectionStart = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(80, 3);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(70, 38);
+            this.Paste.TabIndex = 46;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(156, 3);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(70, 38);
+            this.Copy.TabIndex = 45;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(233, 3);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(70, 38);
+            this.Export.TabIndex = 44;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(233, 158);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(70, 38);
+            this.Insert.TabIndex = 43;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(156, 47);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(147, 78);
+            this.Output.TabIndex = 42;
+            this.Output.Text = "";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(3, 47);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(147, 78);
+            this.Input.TabIndex = 36;
+            this.Input.Text = "";
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 3);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 38);
+            this.Open.TabIndex = 35;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 51;
+            this.label2.Text = "Section End";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 50;
+            this.label1.Text = "Section Start";
+            // 
+            // SetLimits
+            // 
+            this.SetLimits.AutoSize = true;
+            this.SetLimits.Location = new System.Drawing.Point(25, 134);
+            this.SetLimits.Name = "SetLimits";
+            this.SetLimits.Size = new System.Drawing.Size(104, 17);
+            this.SetLimits.TabIndex = 49;
+            this.SetLimits.Text = "Set section limits";
+            this.SetLimits.UseVisualStyleBackColor = true;
+            this.SetLimits.CheckedChanged += new System.EventHandler(this.SetLimits_CheckedChanged);
+            // 
+            // SectionEnd
+            // 
+            this.SectionEnd.Enabled = false;
+            this.SectionEnd.Location = new System.Drawing.Point(80, 158);
+            this.SectionEnd.Name = "SectionEnd";
+            this.SectionEnd.Size = new System.Drawing.Size(70, 20);
+            this.SectionEnd.TabIndex = 48;
+            // 
+            // SectionStart
+            // 
+            this.SectionStart.Enabled = false;
+            this.SectionStart.Location = new System.Drawing.Point(4, 158);
+            this.SectionStart.Name = "SectionStart";
+            this.SectionStart.Size = new System.Drawing.Size(70, 20);
+            this.SectionStart.TabIndex = 47;
+            // 
+            // Quantum_Inserter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 202);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetLimits);
+            this.Controls.Add(this.SectionEnd);
+            this.Controls.Add(this.SectionStart);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Quantum_Inserter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox SetLimits;
+        private System.Windows.Forms.TextBox SectionEnd;
+        private System.Windows.Forms.TextBox SectionStart;
+    }
+}
---
+++ SS Multi Tool/Quantum Inserter.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SS_Multi_Tool
+{
+    public partial class Quantum_Inserter : Form
+    {
+        public Quantum_Inserter()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (Output.Text != "")
+            {
+                Clipboard.SetText(Output.Text);
+            }
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Title = "Save file as";
+                saveFileDialog1.ShowDialog();
+                TextWriter txt = new StreamWriter(saveFileDialog1.FileName);
+                txt.Write(Output.Text);
+                txt.Close();
+                MessageBox.Show("The data has been successfully exported.");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = Input.Text;
+                if (data.Contains("https:"))
+                {
+                    try
+                    {
+                        SecureWebClient wc = new SecureWebClient();
+                        data = wc.DownloadString(data);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed to download data from url");
+                    }
+                }
+                if (decimal.TryParse(SectionStart.Text, out _) == false)
+                {
+                    SectionStart.Text = "0";
+                }
+                if (decimal.TryParse(SectionEnd.Text, out _) == false)
+                {
+                    SectionEnd.Text = "0";
+                }
+                decimal x, y, time, x2 = 0, y2 = 0, time2 = 0, xf, yf, timef;
+                string output = data.Substring(0, data.IndexOf(','));
+                data = data.Replace(output + ",", "");
+                string[] newdata = data.Split(',');
+                for (int i = 0; i + 1 < newdata.Count(); i++)
+                {
+                    var lineSplit = Regex.Matches(newdata[i], "([^|]+)");
+                    x = decimal.Parse(lineSplit[0].Value);
+                    y = decimal.Parse(lineSplit[1].Value);
+                    time = decimal.Parse(lineSplit[2].Value);
+                    var lineSplit2 = Regex.Matches(newdata[i + 1], "([^|]+)");
+                    x2 = decimal.Parse(lineSplit2[0].Value);
+                    y2 = decimal.Parse(lineSplit2[1].Value);
+                    time2 = decimal.Parse(lineSplit2[2].Value);
+                    if (SetLimits.Checked == true)
+                    {
+                        if (time >= decimal.Parse(SectionStart.Text) && time2 <= decimal.Parse(SectionEnd.Text))
+                        {
+                            xf = (x2 + x) / 2;
+                            yf = (y2 + y) / 2;
+                            timef = Math.Round((time2 + time) / 2);
+                            output += "," + xf + "|" + yf + "|" + timef;
+                        }
+                    }
+                    else
+                    {
+                        xf = (x2 + x) / 2;
+                        yf = (y2 + y) / 2;
+                        timef = Math.Round((time2 + time) / 2);
+                        output += "," + x + "|" + y + "|" + time + "," + xf + "|" + yf + "|" + timef;
+                    }
+                }
+                output += "," + x2 + "|" + y2 + "|" + time2;
+                Output.Text = output;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void SetLimits_CheckedChanged(object sender, EventArgs e)
+        {
+            SectionStart.Enabled = SetLimits.Checked;
+            SectionEnd.Enabled = SetLimits.Checked;
+        }
+    }
+}
