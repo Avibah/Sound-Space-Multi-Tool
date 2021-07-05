acbac51d1746fbfefed8e27f3c902968c6e99b34
Avibah <58798677+Avibah@users.noreply.github.com>
2021-07-05 16:46:13 -0500

    Add Map Data Extractor

---
+++ SS Multi Tool/Map Data Extractor.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Map_Data_Extractor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Map_Data_Extractor));
+            this.Extract = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Extract
+            // 
+            this.Extract.Location = new System.Drawing.Point(3, 130);
+            this.Extract.Name = "Extract";
+            this.Extract.Size = new System.Drawing.Size(300, 38);
+            this.Extract.TabIndex = 46;
+            this.Extract.Text = "Extract";
+            this.Extract.UseVisualStyleBackColor = true;
+            this.Extract.Click += new System.EventHandler(this.Extract_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(80, 2);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(70, 38);
+            this.Paste.TabIndex = 45;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(156, 2);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(70, 38);
+            this.Copy.TabIndex = 44;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(233, 2);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(70, 38);
+            this.Export.TabIndex = 43;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(156, 46);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(147, 78);
+            this.Output.TabIndex = 42;
+            this.Output.Text = "";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(3, 46);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(147, 78);
+            this.Input.TabIndex = 41;
+            this.Input.Text = "";
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 2);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 38);
+            this.Open.TabIndex = 40;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Map_Data_Extractor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 171);
+            this.Controls.Add(this.Extract);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Map_Data_Extractor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Extract;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ SS Multi Tool/Map Data Extractor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SS_Multi_Tool
+{
+    public partial class Map_Data_Extractor : Form
+    {
+        public Map_Data_Extractor()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Title = "Save as";
+                saveFileDialog1.ShowDialog();
+                TextWriter txt = new StreamWriter(saveFileDialog1.FileName);
+                txt.Write(Output.Text);
+                txt.Close();
+                MessageBox.Show("The data has been successfully exported.");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (Output.Text != "")
+            {
+                Clipboard.SetText(Output.Text);
+            }
+        }
+
+        private void Extract_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = Input.Text;
+                while (data.Contains("https:"))
+                {
+                    SecureWebClient wc = new SecureWebClient();
+                    data = wc.DownloadString(data);
+                }
+                Output.Text = data;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to download data from url");
+            }
+        }
+    }
+}
