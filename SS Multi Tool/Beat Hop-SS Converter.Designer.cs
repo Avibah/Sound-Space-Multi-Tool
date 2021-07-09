fa70f03cc971e9a4756dcd8763b23a9564e8bbd0
Avibah <58798677+Avibah@users.noreply.github.com>
2021-07-09 00:42:01 -0500

    Add Beat Hop-SS Converter

---
+++ SS Multi Tool/Beat Hop-SS Converter.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Beat_Hop_SS_Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Beat_Hop_SS_Converter));
+            this.Convert = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Export = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // Convert
+            // 
+            this.Convert.Location = new System.Drawing.Point(3, 149);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(258, 38);
+            this.Convert.TabIndex = 13;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = true;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(135, 47);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(126, 96);
+            this.Output.TabIndex = 12;
+            this.Output.Text = "";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(3, 47);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(126, 96);
+            this.Input.TabIndex = 11;
+            this.Input.Text = "";
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(201, 3);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(60, 38);
+            this.Export.TabIndex = 10;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(135, 3);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(60, 38);
+            this.Copy.TabIndex = 9;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(69, 3);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(60, 38);
+            this.Paste.TabIndex = 8;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 3);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(60, 38);
+            this.Open.TabIndex = 7;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Beat_Hop_SS_Converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 191);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Beat_Hop_SS_Converter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
---
+++ SS Multi Tool/Beat Hop-SS Converter.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SS_Multi_Tool
+{
+    public partial class Beat_Hop_SS_Converter : Form
+    {
+        public Beat_Hop_SS_Converter()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Title = "Save as";
+                saveFileDialog1.ShowDialog();
+                TextWriter txt = new StreamWriter(saveFileDialog1.FileName);
+                txt.Write(Output.Text);
+                txt.Close();
+                MessageBox.Show("The data has been successfully exported.");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (Output.Text != "")
+            {
+                Clipboard.SetText(Output.Text);
+            }
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = Input.Text;
+                SecureWebClient wc = new SecureWebClient();
+                try
+                {
+                    while (true)
+                    {
+                        data = wc.DownloadString(data);
+                    }
+                }
+                catch
+                {
+
+                }
+                string output = data.Substring(0, data.IndexOf(','));
+                data = data.Replace(output + ",", "");
+                string[] newdata = data.Split(',');
+                List<string> finaldata = new List<string>();
+                decimal x;
+                decimal y = 0;
+                decimal time;
+                decimal check;
+                var rand = new Random();
+                long num = 0L;
+                long num2 = 0L;
+                foreach (var line in newdata)
+                {
+                    var lineSplit = Regex.Matches(line, "([^|]+)");
+                    float num3 = float.Parse(lineSplit[0].Value);
+                    float num4 = float.Parse(lineSplit[1].Value);
+                    long num5 = long.Parse(lineSplit[2].Value);
+                    long num6;
+                    if (num == 0L)
+                    {
+                        num2 = num5;
+                        num6 = num5;
+                    }
+                    else
+                    {
+                        num6 = num2 + num5;
+                        if (num6 != num2)
+                        {
+                            num2 = num6;
+                        }
+                    }
+                    num += num6;
+                    num5 = num;
+                    finaldata.Add(num3 + "|" + num4 + "|" + num5);
+                }
+                foreach (var line in finaldata)
+                {
+                    var lineSplit = Regex.Matches(line, "([^|]+)");
+                    x = decimal.Parse(lineSplit[0].Value);
+                    time = decimal.Parse(lineSplit[2].Value);
+                    x = Math.Round(x);
+                    if (x > 4)
+                    {
+                        x = 4;
+                    }
+                    if (x < 0)
+                    {
+                        x = 0;
+                    }
+                    switch (x)
+                    {
+                        case 0:
+                            x = 0;
+                            y = 0;
+                            break;
+                        case 1:
+                            check = rand.Next(0, 2);
+                            if (check == 0)
+                            {
+                                x = 1;
+                                y = 0;
+                            }
+                            else
+                            {
+                                x = 0;
+                                y = 1;
+                            }
+                            break;
+                        case 2:
+                            check = rand.Next(0, 3);
+                            if (check == 0)
+                            {
+                                x = 2;
+                                y = 0;
+                            }
+                            else if (check == 1)
+                            {
+                                x = 1;
+                                y = 1;
+                            }
+                            else
+                            {
+                                x = 0;
+                                y = 2;
+                            }
+                            break;
+                        case 3:
+                            check = rand.Next(0, 2);
+                            if (check == 0)
+                            {
+                                x = 2;
+                                y = 1;
+                            }
+                            else
+                            {
+                                x = 1;
+                                y = 2;
+                            }
+                            break;
+                        case 4:
+                            x = 2;
+                            y = 2;
+                            break;
+                    }
+                    output += "," + x + "|" + y + "|" + time;
+                }
+                Output.Text = output;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
