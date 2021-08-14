d181fd10e551890b107cc0d770dd87f355ea6887
Avibah <58798677+Avibah@users.noreply.github.com>
2021-08-13 19:33:01 -0500

    Add Angle Definer

---
+++ SS Multi Tool/Angle Definer.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Angle_Definer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Angle_Definer));
+            this.Define = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Copy = new System.Windows.Forms.Button();
+            this.Export = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Define
+            // 
+            this.Define.Location = new System.Drawing.Point(2, 131);
+            this.Define.Name = "Define";
+            this.Define.Size = new System.Drawing.Size(300, 38);
+            this.Define.TabIndex = 60;
+            this.Define.Text = "Define";
+            this.Define.UseVisualStyleBackColor = true;
+            this.Define.Click += new System.EventHandler(this.Define_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(79, 3);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(70, 38);
+            this.Paste.TabIndex = 59;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Copy
+            // 
+            this.Copy.Location = new System.Drawing.Point(155, 3);
+            this.Copy.Name = "Copy";
+            this.Copy.Size = new System.Drawing.Size(70, 38);
+            this.Copy.TabIndex = 58;
+            this.Copy.Text = "Copy";
+            this.Copy.UseVisualStyleBackColor = true;
+            this.Copy.Click += new System.EventHandler(this.Copy_Click);
+            // 
+            // Export
+            // 
+            this.Export.Location = new System.Drawing.Point(232, 3);
+            this.Export.Name = "Export";
+            this.Export.Size = new System.Drawing.Size(70, 38);
+            this.Export.TabIndex = 57;
+            this.Export.Text = "Export";
+            this.Export.UseVisualStyleBackColor = true;
+            this.Export.Click += new System.EventHandler(this.Export_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(155, 47);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(147, 78);
+            this.Output.TabIndex = 56;
+            this.Output.Text = "";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(2, 47);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(147, 78);
+            this.Input.TabIndex = 55;
+            this.Input.Text = "";
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(2, 3);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 38);
+            this.Open.TabIndex = 54;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Angle_Definer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 171);
+            this.Controls.Add(this.Define);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Copy);
+            this.Controls.Add(this.Export);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Open);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Angle_Definer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Define;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Copy;
+        private System.Windows.Forms.Button Export;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ SS Multi Tool/Angle Definer.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace SS_Multi_Tool
+{
+    public partial class Angle_Definer : Form
+    {
+        public Angle_Definer()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Export_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                saveFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+                saveFileDialog1.DefaultExt = "txt";
+                saveFileDialog1.Title = "Save as";
+                saveFileDialog1.ShowDialog();
+                TextWriter txt = new StreamWriter(saveFileDialog1.FileName);
+                txt.Write(Output.Text);
+                txt.Close();
+                MessageBox.Show("The data has been successfully exported.");
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            if (Output.Text != "")
+            {
+                Clipboard.SetText(Output.Text);
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Define_Click(object sender, EventArgs e)
+        {
+            string data = Input.Text;
+            SecureWebClient wc = new SecureWebClient();
+            try
+            {
+                while (true)
+                {
+                    data = wc.DownloadString(data);
+                }
+            }
+            catch
+            {
+
+            }
+            string output = data.Substring(0, data.IndexOf(','));
+            data = data.Replace(output + ",", "");
+            output = "";
+            double x;
+            double nextx;
+            double y;
+            double nexty;
+            double time;
+            double diffx;
+            double diffy;
+            double angle;
+            string prevloc = "";
+            string[] newdata = data.Split(',');
+            for (int i = 0; i < newdata.Count() - 1; i++)
+            {
+                var lineSplit = Regex.Matches(newdata[i], "([^|]+)");
+                x = double.Parse(lineSplit[0].Value);
+                x = x * -1 + 2;
+                y = double.Parse(lineSplit[1].Value);
+                var nextlineSplit = Regex.Matches(newdata[i + 1], "([^|]+)");
+                nextx = double.Parse(nextlineSplit[0].Value);
+                nextx = nextx * -1 + 2;
+                nexty = double.Parse(nextlineSplit[1].Value);
+                diffx = nextx - x;
+                diffy = nexty - y;
+                time = double.Parse(nextlineSplit[2].Value);
+                if (prevloc != x + "|" + y)
+                {
+                    angle = Math.Round(Math.Atan2(diffy, diffx) * (180 / Math.PI), 2);
+                    if (angle < 0)
+                    {
+                        angle += 360;
+                    }
+                    output += "\n" + angle + " | " + time;
+                    prevloc = x + "|" + y;
+                }
+            }
+            output = output.Substring(1, output.Length - 1);
+            Output.Text = output;
+        }
+    }
+}
