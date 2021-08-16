d8e4500ae70bd4cfb1a1cd165dfba9ac4708200a
Avibah <58798677+Avibah@users.noreply.github.com>
2021-08-16 17:29:30 -0500

    Add BPM line snapping

--- SS Multi Tool/Map Stacker.Designer.cs
+++ SS Multi Tool/Map Stacker.Designer.cs
@@ -217,7 +217,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 399);
+            this.ClientSize = new System.Drawing.Size(306, 224);
             this.Controls.Add(this.Divisor);
             this.Controls.Add(this.DivisorLabel);
             this.Controls.Add(this.label3);
