249af11c34f38b37266f341153c630e291eb2470
Avibah <58798677+Avibah@users.noreply.github.com>
2021-08-23 15:35:18 -0500

    Add Map Note Counter

---
+++ SS Multi Tool/Map Note Counter.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace SS_Multi_Tool
+{
+    partial class Map_Note_Counter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Map_Note_Counter));
+            this.Count = new System.Windows.Forms.Button();
+            this.Paste = new System.Windows.Forms.Button();
+            this.Open = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Count
+            // 
+            this.Count.Location = new System.Drawing.Point(3, 131);
+            this.Count.Name = "Count";
+            this.Count.Size = new System.Drawing.Size(146, 23);
+            this.Count.TabIndex = 17;
+            this.Count.Text = "Count";
+            this.Count.UseVisualStyleBackColor = true;
+            this.Count.Click += new System.EventHandler(this.Count_Click);
+            // 
+            // Paste
+            // 
+            this.Paste.Location = new System.Drawing.Point(79, 3);
+            this.Paste.Name = "Paste";
+            this.Paste.Size = new System.Drawing.Size(70, 38);
+            this.Paste.TabIndex = 16;
+            this.Paste.Text = "Paste";
+            this.Paste.UseVisualStyleBackColor = true;
+            this.Paste.Click += new System.EventHandler(this.Paste_Click);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(3, 3);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(70, 38);
+            this.Open.TabIndex = 15;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(3, 160);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(146, 20);
+            this.Output.TabIndex = 14;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(3, 47);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(146, 78);
+            this.Input.TabIndex = 13;
+            this.Input.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Map_Note_Counter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(152, 183);
+            this.Controls.Add(this.Count);
+            this.Controls.Add(this.Paste);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Map_Note_Counter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Count;
+        private System.Windows.Forms.Button Paste;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.RichTextBox Input;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
---
+++ SS Multi Tool/Map Note Counter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SS_Multi_Tool
+{
+    public partial class Map_Note_Counter : Form
+    {
+        public Map_Note_Counter()
+        {
+            InitializeComponent();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Text Documents (*.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+            string filename = openFileDialog1.FileName;
+            try
+            {
+                string readfile = File.ReadAllText(filename);
+                Input.Text = readfile;
+                if (Input.Text.Substring(0, 2) == "PK")
+                {
+                    MessageBox.Show("Please extract the '.zip' file before attempting to load files from it.", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    Input.Text = "";
+                }
+                else if (Input.Text.Length < 10)
+                {
+                    MessageBox.Show("No valid file was selected for loading.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Input.Text = "";
+                }
+            }
+            catch
+            {
+                Input.Text = "";
+            }
+        }
+
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            Input.Text = Clipboard.GetText();
+        }
+
+        private void Count_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = Input.Text;
+                SecureWebClient wc = new SecureWebClient();
+                try
+                {
+                    while (true)
+                    {
+                        data = wc.DownloadString(data);
+                    }
+                }
+                catch
+                {
+
+                }
+                data = data.Replace(data.Substring(0, data.IndexOf(',')), "");
+                data = data.Substring(1, data.Length - 1);
+                Output.Text = data.Split(',').Count().ToString();
+            }
+            catch
+            {
+                Output.Text = "0";
+            }
+        }
+    }
+}
