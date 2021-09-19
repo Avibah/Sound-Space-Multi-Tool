bf3002c29176fb97dfd318c6ec970552c5e4c4d2
Avibah <58798677+Avibah@users.noreply.github.com>
2021-09-19 03:37:46 -0500

    Update changelog

--- SS Multi Tool/Form1.Designer.cs
+++ SS Multi Tool/Form1.Designer.cs
@@ -55,8 +55,8 @@
             this.Changelog.ReadOnly = true;
             this.Changelog.Size = new System.Drawing.Size(240, 283);
             this.Changelog.TabIndex = 3;
-            this.Changelog.Text = "SS Multi Tool v1.17\n\n-Added \'Map Note Counter\'\n\n-Added a BPM setting to the \'SS-O" +
-    "SU Converter\'";
+            this.Changelog.Text = "SS Multi Tool v1.17.1\n\n-Added international support for \'Map Mirrorer\' and \'Map R" +
+    "otator\'";
             // 
             // OpenGit
             // 
